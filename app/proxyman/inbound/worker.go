88148b1af420559b581d54e2c51ad42a97568e9a
Julian Liu <wlootlxt123@gmail.com>
2021-05-21 00:11:03 +0800

    Tune for udp

--- app/proxyman/inbound/worker.go
+++ app/proxyman/inbound/worker.go
@@ -353,7 +353,7 @@ func (w *udpWorker) clean() error {
 	}
 
 	for addr, conn := range w.activeConn {
-		if nowSec-atomic.LoadInt64(&conn.lastActivityTime) > 8 { // TODO Timeout too small
+		if nowSec-atomic.LoadInt64(&conn.lastActivityTime) > 300 {
 			if !conn.inactive {
 				conn.setInactive()
 				delete(w.activeConn, addr)
@@ -378,7 +378,7 @@ func (w *udpWorker) Start() error {
 	}
 
 	w.checker = &task.Periodic{
-		Interval: time.Second * 16,
+		Interval: time.Minute,
 		Execute:  w.clean,
 	}
 
