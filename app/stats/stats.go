4c6dbd2783b62b0bbbcf7aca223821e0300ecb29
Julian Liu <wlootlxt123@gmail.com>
2021-11-20 21:53:23 +0800

    Make userip more smooth

--- app/stats/stats.go
+++ app/stats/stats.go
@@ -17,7 +17,23 @@ type Mapper struct {
 }
 
 func (m *Mapper) Add(k string, v int) {
-	m.kv.Store(k, v)
+	stable := 0
+	value, ok := m.kv.Load(k)
+	if ok {
+		stable = value.([]int)[1]
+		switch stable {
+		case 2:
+			stable = 1
+		case 0:
+			delta := v - value.([]int)[0]
+			if delta >= 20 {
+				stable = 1
+			} else if delta < 150 {
+				v = value.([]int)[0]
+			}
+		}
+	}
+	m.kv.Store(k, []int{v, stable})
 }
 
 func (m *Mapper) Del(k string) {
@@ -26,13 +42,16 @@ func (m *Mapper) Del(k string) {
 
 func (m *Mapper) TrimAndGet() []string {
 	var ips []string
-	deadline := int(time.Now().Unix()) - 300
+	deadline := int(time.Now().Unix()) - 150
 
 	m.kv.Range(func(key interface{}, value interface{}) bool {
-		if value.(int) < deadline {
-			m.Del(key.(string))
-		} else {
-			ips = append(ips, key.(string))
+		k := key.(string)
+		v := value.([]int)
+		if v[0] < deadline {
+			m.kv.Delete(k)
+		} else if v[1] == 1 {
+			ips = append(ips, k)
+			m.kv.Store(k, []int{v[0], 2})
 		}
 		return true
 	})
